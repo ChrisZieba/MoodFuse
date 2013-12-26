55523d472bea857983ed4b79a5990b942db2751d
Chris Zieba <zieba.chris@gmail.com>
2013-12-25 16:02:15 -0800

    Add 404 and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ MoodFuse::Application.routes.draw do
 	root :to => 'home#index'
 	match '/terms', :controller => 'home', :action => 'terms'
 	match '/privacy', :controller => 'home', :action => 'privacy'
-
 	match '/about', :controller => 'home', :action => 'about'
 	
 end
