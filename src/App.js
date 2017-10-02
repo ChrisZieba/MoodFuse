4a74a79b466537e9b0507511a48b645e9baec336
Chris Zieba <zieba.chris@gmail.com>
2017-10-02 02:11:42 -0700

    Get everything working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/public/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  }
+};
